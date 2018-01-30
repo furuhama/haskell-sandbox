e890cf4271c17abee18b441af71df149139530a7
furuhama <furuhama@users.noreply.github.com>
2018-01-30 18:19:10 +0900

    Add fib

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,11 @@ fact :: Int -> Int
 fact 0 = 1
 fact n = n * fact (n-1)
 
+fib :: Int -> Int
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n-1) + fib (n-2)
+
 main = do
     -- definition of local variables
     let b = 10
@@ -26,3 +31,5 @@ main = do
     if b == 1 then print "hoge" else print "fuga"
 
     print (fact 10)
+
+    print(fib 10)
