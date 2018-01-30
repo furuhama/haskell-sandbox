d81613c3aac298dcd52b65a7e172de4a09d4ab3e
furuhama <furuhama@users.noreply.github.com>
2018-01-30 15:51:01 +0900

    Add if & pattern matching

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,22 @@
 module Main where
 
 -- import Lib
-
 -- main :: IO ()
+
+-- functions
+head' :: [a] -> a
+head' [] = error "List is Empty."
+head' (x:xs) = x
+
 main = do
+    -- definition of local variables
+    let b = 10
+        ls = ["nyan", "nyanko", "nyanpo"]
+
+    -- actual process
     print "Hello, Haskell!"
-    print "hoge hoge"
+
+    print (head' ls)
+
+    if b == 1 then print "hoge" else print "fuga"
+
