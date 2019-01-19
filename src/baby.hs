ba7cb7ebe47b62462ab03e3fa9199a84985b3a9b
furuhama <furuhama@users.noreply.github.com>
2019-01-19 22:17:25 +0900

    Add doubleSmallNum

--- src/baby.hs
+++ src/baby.hs
@@ -6,3 +6,7 @@ doubleUs x y = x*2 + y*2
 
 -- doubleUs with doubleMe function
 doubleUs2 x y = doubleMe x + doubleMe y
+
+doubleSmallNum x = if x > 100
+                      then x
+                      else x * 2
