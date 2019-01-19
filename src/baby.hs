c74b1ade8f4bd493eb10b695cbb2b1131244657f
furuhama <furuhama@users.noreply.github.com>
2019-01-19 22:28:10 +0900

    Add some list operators

--- src/baby.hs
+++ src/baby.hs
@@ -10,3 +10,17 @@ doubleUs2 x y = doubleMe x + doubleMe y
 doubleSmallNum x = if x > 100
                       then x
                       else x * 2
+
+-- `'` is a valid character for variable names
+conanO'Brien = "It's a-me, Conan O'Brien"
+
+-- you can concatinate lists with `++` operator
+lists = [1,2,3,4] ++ [5,6,7,8] -- [1,2,3,4,5,6,7,8]
+
+-- `:` is a cons operator
+lists2 = 'A':" SMALL CAT" -- "A SMALL CAT"
+lists3 = 5:[1,2,3,4] -- [5,1,2,3,4]
+
+-- `!!` is a index operator
+e = "Steve Buscemi" !! 6 -- 'B'
+e2 = [1,2,3,4] !! 1 -- 2
