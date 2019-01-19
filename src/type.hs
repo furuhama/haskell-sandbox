9a7614e0cfbb5faaf99f411c4a9edb46d198ad21
furuhama <furuhama@users.noreply.github.com>
2019-01-19 23:45:58 +0900

    Add type.hs

---
+++ src/type.hs
@@ -0,0 +1,15 @@
+-- type.hs
+
+removeNonUppercase :: [Char] -> [Char]
+removeNonUppercase st = [c | c <- st, c `elem` ['A'..'Z']]
+
+addTwo :: Int -> Int -> Int
+addTwo x y = x + y
+
+-- Int is a 32 bit integer
+fact :: Int -> Int
+fact n = product [1..n]
+
+-- Integer is non limited integer
+fact' :: Integer -> Integer
+fact' n = product [1..n]
