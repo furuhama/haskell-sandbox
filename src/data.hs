50e7d84227197836919ac5152f0c8a4f00770a5f
furuhama <furuhama@users.noreply.github.com>
2019-05-26 21:57:01 +0900

    Define new data type

--- src/data.hs
+++ src/data.hs
@@ -8,3 +8,10 @@ data Day = Sunday | Monday | Tuesday | Wednesday | Thursday | Friday | Saturday
 data Point = Point (Double, Double) deriving (Show, Read)
 
 data Triangle = Triangle Point Point Point deriving (Show, Read) -- the gravity center & two vertexes
+
+data Shape = Circle Float Float Float | Rectangle Float Float Float Float
+    deriving Show
+
+surface :: Shape -> Float
+surface (Circle _ _ r) = pi * r ^ 2
+surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
