c1e56a29c02165b93f832526c4b597ea09a8cb98
furuhama <furuhama@users.noreply.github.com>
2019-05-26 21:33:49 +0900

    Add chain function and `$`, `.` operator usages

--- src/higher_func.hs
+++ src/higher_func.hs
@@ -32,3 +32,27 @@ filter' f (x:xs)
     | otherwise = filter' f xs
 -- filter' (>3) [1,5,3,57,2,5,7,3,1,3]
 -- [5,57,5,7]
+
+chain :: (Integral a) => a -> [a]
+chain 1 = [1]
+chain n
+    | even n = n:chain (n `div` 2)
+    | odd n = n:chain (n*3 + 1)
+
+numLongChains :: Int
+numLongChains = length (filter (\xs -> length xs > 15) (map chain [1..100]))
+
+sum'' :: (Num a) => [a] -> a
+sum'' xs = foldl (\acc x -> acc + x) 0 xs
+
+-- $ operator means function application
+-- use like this
+-- maximum (map length (map chain [1..100]))
+-- =>
+-- maximum $ map length $ map chain [1..100]
+
+-- . operator compose functions
+-- use like this
+-- maximum $ map length $ map chain [1..100]
+-- =>
+-- maximum $ map (length . chain) [1..100]
